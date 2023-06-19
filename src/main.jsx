3c3049c994f43a353f962b58260ae547d56980cc
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-06-19 15:43:48 -0700

    sdasdasda

--- src/main.jsx
+++ src/main.jsx
@@ -5,31 +5,31 @@ import './index.css'
 import { configureStore } from '@reduxjs/toolkit';
 import basketSlice from './reducers/basketSlice';
 import { Provider } from 'react-redux';
-import { transitions, positions, Provider as AlertProvider, types } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
+// import { transitions, positions, Provider as AlertProvider, types } from 'react-alert';
+// import AlertTemplate from 'react-alert-template-basic';
 
 
 const store = configureStore({
   reducer: basketSlice
 });
 
-const options = {
-  // you can also just use 'bottom center'
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
-  // you can also just use 'scale'
-  transition: transitions.SCALE,
-  type:types.SUCCESS
-}
+// const options = {
+//   // you can also just use 'bottom center'
+//   position: positions.BOTTOM_CENTER,
+//   timeout: 5000,
+//   offset: '30px',
+//   // you can also just use 'scale'
+//   transition: transitions.SCALE,
+//   type:types.SUCCESS
+// }
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AlertProvider template={AlertTemplate} {...options}>
+      {/* <AlertProvider template={AlertTemplate} {...options}> */}
         <App />
-      </AlertProvider>
+      {/* </AlertProvider> */}
     </Provider>
   </React.StrictMode>,
 )
